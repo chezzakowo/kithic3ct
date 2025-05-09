50bcce6ede8585700fe4b02e783cf805842d0686
Chez_ <129508826+chezzakowo@users.noreply.github.com>
2025-05-09 17:44:19 +0700

    Chuyển hướng nhầm sang root

--- js/rth.js
+++ js/rth.js
@@ -1,5 +1,5 @@
 console.log('⏳ Đợi 5 giây trước khi chuyển trang...');
 setTimeout(function () {
     console.log('🔙 Đang chuyển trang');
-    window.location.href = '/';
+    window.location.href = '/kithic3ct';
 }, 5000);
